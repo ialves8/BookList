4d31afa7b6582fab3ca8587a034f5e713d3daeed
Iranildo Ferreira Alves <ialves8@hotmail.com>
2019-02-06 12:47:34 -0200

    Add project files.

---
+++ BookList/Migrations/20181230024000_updateNameProperty.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookList.Migrations
+{
+    public partial class updateNameProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_books",
+                table: "books");
+
+            migrationBuilder.RenameTable(
+                name: "books",
+                newName: "Books");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Books",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Books",
+                table: "Books",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Books",
+                table: "Books");
+
+            migrationBuilder.RenameTable(
+                name: "Books",
+                newName: "books");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "books",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_books",
+                table: "books",
+                column: "Id");
+        }
+    }
+}
